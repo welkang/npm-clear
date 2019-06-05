8ebafef7094011fa22073a5af3ffc6daade90ab3
寻卓 <huankang.hhk@koubei.com>
2019-06-06 01:08:11 +0800

    feat: 精简文案

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const exec = require('child_process').execSync;
 const targetFoler = 'node_modules';
 
 program
-  .version('0.0.3')
+  .version('0.0.4')
   .parse(process.argv);
 
 if (program.args && program.args[0]) {
-  const listShell = `find ${path.resolve(program.args[0])} -name ${targetFoler}`;
-  const listResult = exec(listShell);
-  exec(`find ${path.resolve(program.args[0])} -name ${targetFoler} | xargs rm -rf`);
-  console.log(`已成功移除以下 ${targetFoler} 文件夹：`);
-  console.log(listResult.toString("utf8").trim());
+  const currentPath = path.resolve(program.args[0]);
+  exec(`find ${currentPath} -name ${targetFoler} | xargs rm -rf`);
+  console.log(`已成功移除 ${currentPath} 下的所有 ${targetFoler} 文件夹！`);
 } else {
   console.log('请指定目录');
 }
